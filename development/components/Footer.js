7e341dfc7c9d5b5e136b0669eab9e6bc244ac8a9
fraorzi <orzechowskifranek@gmail.com>
2023-08-18 17:18:17 +0200

    szybka zmiana

--- development/components/Footer.js
+++ development/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-            {/* Możesz tu dodać informacje o autorze, linki do mediów społecznościowych itp. */}
+            {}
         </footer>
     );
 }
--- development/components/Header.js
+++ development/components/Header.js
@@ -16,15 +16,15 @@ function Header() {
     };
 
     return (
-        <header className="fixed-header">  {/* Dodajemy tutaj klasę */}
+        <header className="fixed-header">  {}
             <div className="logo">
-                PlanIt   {/* Nazwa aplikacji */}
+                PlanIt   {}
             </div>
             <nav>
                 <ul>
                     <li><a href="/">Strona główna</a></li>
                     <li><a href="/kontakt">Kontakt</a></li>
-                    {/* Możesz dodać więcej linków nawigacyjnych w przyszłości */}
+                    {}
                 </ul>
             </nav>
             <div className="search-bar">
@@ -32,13 +32,13 @@ function Header() {
             </div>
             <div className="actions">
                 <button>Zaloguj się</button>
-                {/* Możesz dodać więcej przycisków akcji w przyszłości */}
+                {}
             </div>
             <div className="user-info">
-                {/* Tutaj będzie wyświetlane imię użytkownika i avatar, jeśli jest zalogowany */}
+                {}
             </div>
             <div className="theme-switcher">
-                {/* Przełącznik motywu */}
+                {}
                 <label>
                     Jasny
                     <input type="checkbox" onChange={toggleTheme} checked={darkMode} />
