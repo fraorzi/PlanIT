f19df1fdc3197ff609275fd5a89b1c0ad99a504e
fraorzi <orzechowskifranek@gmail.com>
2023-08-25 16:15:07 +0200

    zmiana usun na ikone w dniu

--- development/components/Day.js
+++ development/components/Day.js
@@ -67,23 +67,35 @@ function DayComponent({ tasks, meals }) {
             <h2>{currentDay.toLocaleDateString()}</h2>
             <table>
                 <thead>
-                    <tr>
-                        <th>Godzina</th>
-                        <th>Zadania</th>
-                        <th>Posiłki</th>
-                    </tr>
+                <tr>
+                    <th>Godzina</th>
+                    <th>Zadania</th>
+                    <th>Posiłki</th>
+                </tr>
                 </thead>
                 <tbody>
                     {hours.map(hour => (
                         <tr key={hour}>
                             <td>{hour}</td>
                             <td onClick={() => handleAddTask(hour)}>
+                                <div className="task-container">
                                 {internalTasks[hour] || "Kliknij, aby dodać"}
-                                {internalTasks[hour] && <span onClick={(e) => { e.stopPropagation(); handleRemoveTask(hour); }}> [Usuń]</span>}
+                                {internalTasks[hour] && <span className="delete-holiday-btn" onClick={(e) => { e.stopPropagation(); handleRemoveTask(hour); }}>
+                                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M3 3L21 21M18 6L17.6 12M17.2498 17.2527L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6H4M16 6L15.4559 4.36754C15.1837 3.55086 14.4194 3 13.5585 3H10.4416C9.94243 3 9.47576 3.18519 9.11865 3.5M11.6133 6H20M14 14V17M10 10V17" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                    </svg>
+                                </span>}
+                                </div>
                             </td>
                             <td onClick={() => handleAddMeal(hour)}>
+                                <div className="task-container">
                                 {internalMeals[hour] || "Kliknij, aby dodać"}
-                                {internalMeals[hour] && <span onClick={(e) => { e.stopPropagation(); handleRemoveMeal(hour); }}> [Usuń]</span>}
+                                {internalMeals[hour] && <span className="delete-holiday-btn" onClick={(e) => { e.stopPropagation(); handleRemoveMeal(hour); }}>
+                                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M3 3L21 21M18 6L17.6 12M17.2498 17.2527L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6H4M16 6L15.4559 4.36754C15.1837 3.55086 14.4194 3 13.5585 3H10.4416C9.94243 3 9.47576 3.18519 9.11865 3.5M11.6133 6H20M14 14V17M10 10V17" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                    </svg>
+                                </span>}
+                                </div>
                             </td>
                         </tr>
                     ))}
@@ -101,7 +113,7 @@ function DayComponent({ tasks, meals }) {
                 </div>
                 {modalContent === 'task' && <TaskList onTaskSelected={handleTaskSelected} inModal={true} />}
                 {modalContent === 'meal' && <MealList onMealSelected={handleMealSelected} inModal={true} />}
-            </Modal>
+                </Modal>
         </div>
     );
 }
